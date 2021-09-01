1f6a52bb09355dc1f54957381d2c75f2362f6ba7
Veves <vevespm@gmail.com>
2021-09-01 10:51:21 +0200

    comaprenumbers

---
+++ js/main.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const number = document.querySelector(".js_guess_number");
+const randomNumber= getRandomNumber(100);
+const tryIt= document.querySelector(".try");
+const clue= document.querySelector(".clue");
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+    }
+
+console.log("El número generado aleatoriamente es "+ randomNumber);
+
+function compareNumbers(){
+    const guessNumber=  number.value;
+  if (guessNumber < 1 || guessNumber >= 100) {
+    clue.innerHTML='El número debe estar entre 1 y 100';
+  } else if (guessNumber === randomNumber) {
+    clue.innerHTML='Has ganado campeona!!!';
+  } else if (guessNumber > randomNumber) {
+    clue.innerHTML='Demasiado alto';
+  } else if (guessNumber < randomNumber) {
+    clue.innerHTML='Demasiado bajo';
+  }
+}
+function handleButton(){
+    compareNumbers();
+}
+
+tryIt.addEventListener('click', handleButton);
